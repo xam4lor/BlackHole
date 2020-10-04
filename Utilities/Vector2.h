ca796fa292253d1dbec7f5f903a5296ce6fac94b
xam4lor <xam4lor@gmail.com>
2020-10-04 19:44:20 +0200

    Update Vector2.h

--- Utilities/Vector2.h
+++ Utilities/Vector2.h
@@ -16,8 +16,12 @@ class Vector2: public sf::Vector2f
         Vector2(sf::Vector2f vec);
         Vector2(Vector2& vec);
 
+        Vector2 copy();
         Vector2 setMag(float m);
         sf::Vector2f setAngle(float alpha);
+        
+        Vector2 mult(float c);
+        Vector2 div(float c);
 
         static float getMag(Vector2 vec);
         static float getAngle(Vector2 vec1, Vector2 vec2);
